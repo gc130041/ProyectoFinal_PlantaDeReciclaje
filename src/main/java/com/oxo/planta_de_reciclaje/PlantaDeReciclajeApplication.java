9935dedfa1c13c4bd8e6483ea673aed991ceb19b
OlivMer765 <omerida-2021641@kinal.edu.gt>
2025-10-06 18:47:10 -0600

    feat: Añadir proyecto inicial de Spring Boot

---
+++ src/main/java/com/oxo/planta_de_reciclaje/PlantaDeReciclajeApplication.java
@@ -0,0 +1,13 @@
+package com.oxo.planta_de_reciclaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantaDeReciclajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantaDeReciclajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oxo/planta_de_reciclaje/PlantaDeReciclajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxo.planta_de_reciclaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantaDeReciclajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
